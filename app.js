f0297e5ec4afbe0670dfc902765a9e28c2271132
greatt8696 <88331836+greatt8696@users.noreply.github.com>
2022-12-08 17:13:58 +0900

    123

--- app.js
+++ app.js
@@ -4,12 +4,22 @@ const mongoDb = require("mongoose");
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const ether = require("./routers/ether");
+
 const {
   web3,
   getAllBlocks,
   getTransactionManager,
   autoContractTanscation,
 } = require("./web3Manager");
+
+// Web3 init
+(async () => {
+  const blocks = await getAllBlocks();
+  // console.log(blocks);
+  const transactionManager = await getTransactionManager();
+  autoContractTanscation(web3);
+})();
 
 app.use(
   cors({
@@ -18,9 +28,12 @@ app.use(
   })
 );
 
-(async () => {
-  const blocks = await getAllBlocks();
-  // console.log(blocks);
-  const transactionManager = await getTransactionManager();
-  autoContractTanscation(web3);
-})();
+app.use(express.static("public"));
+
+app.listen(3000, () => console.log("SERVER RUNNING"));
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/web3", ether);
--- routers/ether.js
+++ routers/ether.js
@@ -1,11 +1,15 @@
 const router = require("express").Router();
 const tempBlocks = require("../blocks");
 const event = require("../events.json");
+const { getAllBlocks } = require("../web3Manager");
 
-router.get("/getBlocks", (req, res) => {
-  res.send(tempBlocks);
+router.get("/getBlocks", async (req, res) => {
+  const allBlockFromWeb3 = await getAllBlocks();
+  res.send(allBlockFromWeb3);
 });
 
-router.get("/getEvent", (req, res) => {
+router.get("/getEvent", async (req, res) => {
   res.send(event);
 });
+
+module.exports = router;
